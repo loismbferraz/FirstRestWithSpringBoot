6c6445d2540210950ec6262223c42b227afda5f0
Luis Ferraz <luis.ferraz@simbolos.desafios.com>
2021-08-21 12:18:26 +0100

    Hello SpringBoot and Rest

---
+++ 00 FirstRestWithSpringBoot Spring Initializr/src/test/java/com/tutorial/firstrestwithspringboot/FirstRestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.firstrestwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
